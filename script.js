b7e8b494f1710338e6b7c8625a3193c65385971b
Carl Carter <carl.carter@carlcar-ltm38ry.internal.salesforce.com>
2025-02-08 14:06:04 +0000

    updated with sections

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleSection(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
